d7f41fd3db4811c444a56491bb592896680cec43
Amir <amir.nagri+git@gmail.com>
2015-03-10 13:11:20 +0530

    second problem

--- src/com/shankara/bill/Bill.java
+++ src/com/shankara/bill/Bill.java
@@ -1,25 +1,29 @@
 package com.shankara.bill;
 //https://gist.github.com/anagri/cfe78516a69358325230
+//https://gist.github.com/anagri/64888976de0b3bf8e29f
 
 import java.util.ArrayList;
 
 public class Bill {
+    public static void main(String[] args) {
+        Store store = new Store();
+        store.add("Quaker", 1);
+        store.add("Kellogs", 2);
+        store.add("Milk", 4);
+        store.add("Cashew", 5);
+        Bill bill = new Bill(store);
+        bill.add("Quaker", 10);
+        bill.add("Kellogs", 5);
+        bill.add("Milk", 5);
+        bill.add("Cashew", 10);
 
+        bill.printBill();
+    }
 
     private final ArrayList<BillItem> billItems;
 
-    public Bill() {
+    public Bill(Store store) {
         billItems = new ArrayList<>();
-    }
-
-    public static void main(String[] args) {
-        Bill bill = new Bill();
-        bill.add(new BillItem("Quaker", 1, 10));
-        bill.add(new BillItem("Kellogs", 2, 5));
-        bill.add(new BillItem("Milk", 4, 5));
-        bill.add(new BillItem("Cashew", 5, 10));
-
-        bill.printBill();
     }
 
     private void printBill() {
@@ -31,8 +35,8 @@ public class Bill {
         System.out.println(String.format("Grand Total\t\t\t\t\t%d", grandTotal));
     }
 
-    private void add(BillItem billItem) {
-        billItems.add(billItem);
+    private void add(String name, int quantity) {
+        billItems.add(); //TODO
     }
 
     private static class BillItem {
@@ -52,6 +56,12 @@ public class Bill {
 
         public int total() {
             return price * quantity;
+        }
+    }
+
+    private static class Store {
+        public void add(String quaker, int price) {
+            // TODO
         }
     }
 }
