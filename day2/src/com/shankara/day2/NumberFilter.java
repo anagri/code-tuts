4af7fc0b9c0d72fe9e759163ff4edb2f24386246
Amir <amir.nagri+git@gmail.com>
2015-03-16 21:34:11 +0530

    day 2 - lambda solution

--- day2/src/com/shankara/day2/NumberFilter.java
+++ day2/src/com/shankara/day2/NumberFilter.java
@@ -1,24 +1,54 @@
 package com.shankara.day2;
 
+import java.util.ArrayList;
+
 public class NumberFilter {
 
-    public NumberFilter(Integer[] numbers) {
+    private final Integer[] numbers;
 
+    public NumberFilter(Integer[] numbers) {
+        this.numbers = numbers;
     }
 
     public Integer[] even() {
-        return null;
+        return filter(i -> i % 2 == 0);
     }
 
     public Integer[] odd() {
-        return null;
+        return filter(i -> i % 2 == 1);
     }
 
     public Integer sum() {
-        return null;
+        return reduce(0, (i, j) -> i + j);
     }
 
     public Integer mult() {
-        return null;
+        return reduce(1, (i, j) -> i * j);
+    }
+
+    private Integer reduce(int init, Inject inject) {
+        int result = init;
+        for (Integer number : numbers) {
+            result = inject.inject(result, number);
+        }
+        return result;
+    }
+
+    private Integer[] filter(Predicate predicate) {
+        ArrayList<Integer> evens = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (predicate.evaluate(number)) evens.add(number);
+        }
+        Integer[] result = new Integer[evens.size()];
+        evens.toArray(result);
+        return result;
+    }
+
+    private static interface Predicate {
+        public boolean evaluate(int i);
+    }
+
+    private static interface Inject {
+        public int inject(int i, int j);
     }
 }
--- day2/src/com/shankara/day2/NumberFilterApp.java
+++ day2/src/com/shankara/day2/NumberFilterApp.java
@@ -8,7 +8,13 @@ public class NumberFilterApp {
         NumberFilter numberFilter = new NumberFilter(numbers);
         System.out.println(Arrays.toString(numberFilter.even())); // prints 2, 4, 6, 8, 10
         System.out.println(Arrays.toString(numberFilter.odd())); // prints 1, 3, 5, 7, 9
-        System.out.println(numberFilter.sum()); // prints 45
+        System.out.println(numberFilter.sum()); // prints 55
         System.out.println(numberFilter.mult()); // prints 3628800
+
+        System.out.println("Lambda inbuilt functions");
+        System.out.println(Arrays.toString(Arrays.stream(numbers).filter(integer -> integer % 2 == 0).toArray()));
+        System.out.println(Arrays.toString(Arrays.stream(numbers).filter(integer -> integer % 2 == 1).toArray()));
+        System.out.println(Arrays.stream(numbers).reduce(0, (integer, integer2) -> integer + integer2));
+        System.out.println(Arrays.stream(numbers).reduce(1, (integer, integer2) -> integer * integer2));
     }
 }
